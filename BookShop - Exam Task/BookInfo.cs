e9aaccdab1e54946c971020e980cde0ea036a8ad
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-03-29 14:48:14 +0300

    Add BookInfo form

---
+++ BookShop - Exam Task/BookInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BookShop.Entities;
+
+namespace BookShop
+{
+    public partial class BookInfo : Form
+    {
+        List<Genre> genres = new List<Genre>();
+        public Book Book { get; set; }
+        public BookInfo()
+        {
+            InitializeComponent();
+            LoadGenresComboBox();
+        }
+
+        public void LoadGenresComboBox()
+        {
+            genreComboBox.DataSource = null;
+            genreComboBox.DataSource = genres;
+        }
+
+        // TODO
+        private void ConfirmBtnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CancelBtnClick(object sender, EventArgs e)
+        {
+            this.Close();
+            MainPanel form = new MainPanel();
+        }
+    }
+}
