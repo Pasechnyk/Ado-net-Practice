d55a026741d812781c4b5679c83abbe62f7bde73
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2023-03-29 14:47:20 +0300

    Add MainPanel

---
+++ BookShop - Exam Task/MainPanel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BookShop.Entities;
+using BookShop.DatabaseData;
+
+namespace BookShop
+{
+    public partial class MainPanel : Form
+    {
+        List<Book> books = new List<Book>();
+        public MainPanel()
+        {
+            InitializeComponent();
+            UpdateBooksListBox();
+        }
+
+        public void UpdateBooksListBox()
+        {
+            booksListBox.DataSource = null;
+            booksListBox.DataSource = books;
+        }
+
+        private void AddBookBtnClick(object sender, EventArgs e)
+        {
+            BookInfo form = new BookInfo();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                books.Add(form.Book);
+                UpdateBooksListBox();
+            }
+        }
+
+        private void RemoveBookBtnClick(object sender, EventArgs e)
+        {
+            if (booksListBox.SelectedItem == null)
+            {
+                MessageBox.Show("You haven't selected any book to remove!");
+                return;
+            }
+            var item = booksListBox.SelectedItem as Book;
+
+            books.Remove(item);
+
+            UpdateBooksListBox();
+        }
+
+        //TODO
+        private void EditBookBtnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FindByNameBtnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FindByAuthorBtnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FindByGenreBtnClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
